3a67a5db5926b788441276fb5359fe93f5ee29c8
Pauli Marttinen <pauli.marttiin@gmail.com>
2019-09-26 23:03:30 +0300

    Starting to work on the score displays

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <MahjongScoreFields />
+      <MahjongScoreFields initialPoints="30000" />
       <TransactionViews />
     </div>
   );
--- src/MahjongScoreFields.js
+++ src/MahjongScoreFields.js
@@ -6,13 +6,45 @@
 import React from 'react';
 import './MahjongScoreFields.css';
 
-function MahjongScoreFields()
+export class MahjongScoreFields extends React.Component
 {
-    return (
-    <div>
-        <div className="MahjongScoreField" name="AbsolutePoints">aa</div>
-        <div className="MahjongScoreField" name="RelativePoints">aa</div>
-    </div>);
+    constructor(props)
+    {
+        super(props);
+        this.state = { points: this.props.initialPoints };
+    }
+//remember to update the points with
+//  this.setState({points: blahblah});
+//instead of
+//  this.state.points = blahblah;
+
+    formatPoints(points)
+    {
+        var sign;
+        if (points > 0)
+        {
+            sign = "+";
+        }
+        else if (points < 0)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "±";
+        }
+        return <span className="points">{sign}{points/100}<span className="hundreds">00</span></span>;
+    }
+
+    render()
+    {
+        return (
+            <div>
+                <div className="MahjongScoreField" name="AbsolutePoints">{this.formatPoints(this.state.points)}</div>
+                <div className="MahjongScoreField" name="RelativePoints">{this.formatPoints(this.state.points - this.props.initialPoints)}</div>
+            </div>
+        );
+    }
 }
 
 export default MahjongScoreFields;
