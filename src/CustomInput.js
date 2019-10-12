5cc07c86dfe8c5cd90ad38ccea0b4717d0f29a39
Pauli Marttinen <pauli.marttiin@gmail.com>
2019-10-12 16:30:24 +0300

    CustomInput InDec now works

--- src/CustomInput.js
+++ src/CustomInput.js
@@ -8,14 +8,14 @@ class CustomInput extends React.Component
     super(props);
     this.state =
     {
-      customPoints: this.props.points
+      customPoints: parseInt(this.props.points)
     }
   }
 
   changeValue = (newValue) =>
   {
     this.setState({
-      customPoints: newValue
+      customPoints: parseInt(newValue)
     });
   }
 
@@ -23,7 +23,7 @@ class CustomInput extends React.Component
   {
     return (
       <div className="custominput">
-        <IncreaseDecrease value={this.props.points} minimum="noMinimum" increment="100" fastButtons="true" label="Input custom score" formatPoints="true" onClick={this.changeValue} />
+        <IncreaseDecrease value={this.state.customPoints} minimum="noMinimum" increment="100" fastButtons="true" label="Input custom score" formatPoints="true" onClick={this.changeValue} />
         <button data-direction="custom" data-amount={this.state.customPoints} onClick={e => this.props.onClick(e.target)}>Set score</button>
       </div>
     );
