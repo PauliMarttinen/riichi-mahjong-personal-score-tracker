a3e761261ef8dc35b5e7923bf5bc84fb173a372e
Pauli Marttinen <pauli.marttiin@gmail.com>
2019-10-19 00:31:51 +0300

    Make history list look good.

--- src/History.js
+++ src/History.js
@@ -10,39 +10,49 @@ class History extends React.Component
 {
   render()
   {
-    var historyDisplay = [];
-    for (var event = this.props.history.length - 1; event >= 0; event--)
+    var historyDisplay;
+    if (this.props.history.length !== 0)
     {
-      var direction;
-      if (this.props.history[event] < 0)
+      var historyList = [];
+      for (var event = this.props.history.length - 1; event >= 0; event--)
       {
-        direction = "Paid";
-      }
-      else if (this.props.history[event] > 0)
-      {
-        direction = "Received";
-      }
+        var direction;
+        if (this.props.history[event] < 0)
+        {
+          direction = "Paid";
+        }
+        else if (this.props.history[event] > 0)
+        {
+          direction = "Received";
+        }
 
-      historyDisplay.push(
-        <tr key={event} className={"historyevent " + direction.toLowerCase()}>
-          <td>{event + 1}</td>
-          <td>{direction}</td>
-          <td>{formatPoints(this.props.history[event], false)}</td>
-          <td><button data-event={event} onClick={e => this.props.onClick(parseInt(e.target.dataset.event))}>Undo</button></td>
-        </tr>
-      );
+        historyList.push(
+          <tr key={event} className={"historyevent " + direction.toLowerCase()}>
+            <td>{event + 1}</td>
+            <td>{direction}</td>
+            <td>{formatPoints(this.props.history[event], false)}</td>
+            <td><button data-event={event} onClick={e => this.props.onClick(parseInt(e.target.dataset.event))}>Undo</button></td>
+          </tr>
+        );
+      }
+      historyDisplay = 
+        <table className="historytable">
+          <tbody>
+            {historyList}
+          </tbody>
+        </table>;
+    }
+    else
+    {
+      historyDisplay = <p className="label">No transactions to display.</p>;
     }
 
     return (
       <div className="history">
         <div className="label">
-          Change history
+          Transaction history
         </div>
-        <table className="historytable">
-          <tbody>
-            {historyDisplay}
-          </tbody>
-        </table>
+        {historyDisplay}
       </div>
     );
   }
